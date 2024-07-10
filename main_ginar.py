0106a9ba83156429c8f4b3e298a77a357f9f8b0a
Chengqing Yu <114470704+ChengqingYu@users.noreply.github.com>
2024-07-10 14:15:42 +0800

    Update main_ginar.py

--- main_ginar.py
+++ main_ginar.py
@@ -12,8 +12,8 @@ from model1.ginar_arch import GinAR
 from data_solve import load_adj
 import time
 
-plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
-plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+plt.rcParams['font.sans-serif']=['SimHei'] 
+plt.rcParams['axes.unicode_minus']=False 
 
 seed = 42
 random.seed(seed)
